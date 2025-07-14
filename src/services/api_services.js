018390ca00b14bfcd69066feb66e7af4e5562a9f
Incerteza88 <info@incerteza.es>
2025-07-14 10:12:55 +0000

    proyecto finalizado2

--- src/services/api_services.js
+++ src/services/api_services.js
@@ -5,7 +5,6 @@ export const getPeople = (dispatch) => {
     fetch("https://www.swapi.tech/api/people/")
         .then(res => res.json())
         .then(data => {
-            console.log('people:', data)
             dispatch({type: 'setPeople', payload: data.results})
         })
         .catch(err => console.error(err))
